70a4d2008d05e12ab09fcdffa4a82f3505a24604
Sebastian Nicolas <sebas@no-reply.com>
2024-10-09 13:12:28 -0500

    added nvim

---
+++ nvim/lua/chadrc.lua
@@ -0,0 +1,37 @@
+-- This file needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :(
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+  theme = "chadracula-evondev",
+
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+}
+
+M.term = {
+  float = {
+    width = 1,
+    height = 0.9,
+    row = 0,
+    col = 0,
+  },
+}
+
+M.lsp = {
+  signature = true,
+}
+
+M.colorify = {
+  enabled = true,
+  mode = "virtual", -- fg, bg, virtual
+  virt_text = "󱓻 ",
+  highlight = { hex = true, lspvars = true },
+}
+
+return M
