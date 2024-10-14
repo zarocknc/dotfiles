8fde071502e0e69aff5dd23ed4098bf1985433ab
Sebastian Nicolas <sebas@noreply.com>
2024-10-14 08:38:24 -0500

    map lazygit to _gg, italic comments 🤔

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -8,10 +8,10 @@ local M = {}
 M.base46 = {
   theme = "chadracula-evondev",
 
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
 }
 
 M.term = {
--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,7 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<leader>lg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
+map("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
 map("n", "<C-t>", function()
   require("menu").open "default"
 end, { desc = "Abrir Volt Menu" })
